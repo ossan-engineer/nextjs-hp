e72173e85c1ba3196cf47a100f54599c7e6714ad
Kiichi Tachibana <bodega1123@gmail.com>
2021-01-15 21:41:55 +0900

    Add PostItem

---
+++ components/PostItem.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type Props = {
+  post: Post;
+};
+
+const PostItem: React.FC<Props> = ({ post }) => {
+  return (
+    <div>
+      <span>{post.id}</span>:
+      <span className='cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200'>
+        {post.title}
+      </span>
+    </div>
+  );
+};
+
+export default PostItem;
